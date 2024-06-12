6675efb4877f4c0ae585ea57c0ff5f33db93cb6a
tanujkumai <tanujkumai01@gmail.com>
2024-06-12 21:14:45 +0530

    Sync and Async

--- hello_world/file.js
+++ hello_world/file.js
@@ -1,14 +1,37 @@
 const fs = require("fs");
+const os = require("os");
 
-// Sync...
+console.log(os.cpus().length);
+
+// Sync... Blocking.....
 // fs.writeFileSync("./test.txt", "hello world Sync");
 
-//Async
+//Async... Non-Blocking....
 // fs.writeFile("./test.txt", "hello world Async", () => {});
 
 //Sync
+//Dependes on No. of threads. By default threads 4. maxmium threads no of core the serever
+// console.log("1")
 // const result = fs.readFileSync("./contact.txt", "utf-8");
 // console.log(result);
+// console.log("2")
+// // //output
+// // 1
+// // Aman: +91839000000
+// // Tanuj: +91798888888
+// // 2
+
+//Async
+// console.log("1");
+// const result = fs.readFile("./contact.txt", "utf-8", (err, result) => {
+//   console.log(result);
+// });
+// console.log("2");
+// // // output
+// // 1
+// // 2
+// // Aman: +91839000000
+// // Tanuj: +91798888888
 
 //Async
 // fs.readFile("./contact.txt", "utf-8", (err, result) => {
